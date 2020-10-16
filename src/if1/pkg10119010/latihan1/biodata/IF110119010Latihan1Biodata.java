a8016ab4da81a4c6576b99c728d77335cba7ee09
maulana01 <maulanaimammalik4@gmail.com>
2020-10-16 10:24:32 +0700

    new commit

---
+++ src/if1/pkg10119010/latihan1/biodata/IF110119010Latihan1Biodata.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119010.latihan1.biodata;
+
+/**
+ *
+ * @author User
+ */
+public class IF110119010Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "---------------------");
+        System.out.println("NAMA\t\t: Maulana Imam Malik");
+        System.out.println("ALAMAT\t\t: BPP Sankyu Blok G7 No 18 RT 014 RW 003 Kel/Desa Pelamunan Kec.Kramatwatu Kab.Serang Prov.Banten 42161");
+        System.out.println("TEMPAT LAHIR\t: Serang");
+        System.out.println("TANGGAL LAHIR\t: 18 Agustus 2001");
+        System.out.println("---------------------");
+        System.out.println("HOBI\t: Main Game & Tidur");
+        System.out.println("MAKES\t: Cendol");
+        System.out.println("MIKES\t: Dawet");
+    }
+    
+}
